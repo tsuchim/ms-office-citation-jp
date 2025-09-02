5f004a6adaedbc9cc36d4e8522bb4210bca01480
Yuji Tsuchimoto <yuji@eikai.co.jp>
2025-09-03 03:38:55 +0900

    型定義ファイルに引用・文献管理関連のモジュールを追加し、tsconfig.jsonのstrictオプションを有効化しました。

--- ms-office-citation-jp/src/types.d.ts
+++ ms-office-citation-jp/src/types.d.ts
@@ -7,3 +7,10 @@ declare module '*.xml' {
   const content: string;
   export default content;
 }
+
+declare module 'citeproc';
+
+declare module '@citation-js/core';
+declare module '@citation-js/plugin-bibtex';
+declare module '@citation-js/plugin-ris';
+declare module '@citation-js/plugin-csl';
