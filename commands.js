cfc8f0109da24f7c71925e0518fed6810d017ca5
tsuchim <tsuchim@users.noreply.github.com>
2025-09-04 08:27:25 +0000

    deploy: 333dd81689bc85a4c5a5cfa4590b529721aef020

---
+++ commands.js
@@ -0,0 +1,2 @@
+!function(){"use strict";Office.onReady(()=>{}),Office.actions.associate("action",function(e){var i;const o={type:Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,message:"Performed action.",icon:"Icon.80x80",persistent:!0};null===(i=Office.context.mailbox.item)||void 0===i||i.notificationMessages.replaceAsync("ActionPerformanceNotification",o),e.completed()})}();
+//# sourceMappingURL=commands.js.map
